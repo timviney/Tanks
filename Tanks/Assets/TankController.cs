8805249a774e68b70a94a62579f5f3d095026847
Tim Viney <t.c.viney@gmail.com>
2025-04-13 19:32:07 +0100

    Start tanks

---
+++ Tanks/Assets/TankController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float rotationSpeed = 200f;
+    
+    private Rigidbody2D _rb;
+    private Vector2 _movement;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Get WASD input
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Move the tank
+        _rb.linearVelocity = _movement.normalized * moveSpeed;
+
+        // Rotate the tank toward movement direction (only if moving)
+        if (_movement == Vector2.zero) return;
+        
+        var targetAngle = Mathf.Atan2(_movement.y, _movement.x) * Mathf.Rad2Deg - 90f; // -90° to face upward initially
+        var angle = Mathf.LerpAngle(_rb.rotation, targetAngle, rotationSpeed * Time.fixedDeltaTime);
+        _rb.rotation = angle;
+    }
+}
